f036077e15bb6ac2a6528c235cb8cb6f43624a41
hujaepa <hujaepa@gmail.com>
2023-02-03 18:02:03 +0800

    first commit

---
+++ Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Celcius2Fahrenheit
+{
+    class Converter
+    {
+        public static void Main(String[] args)
+        {
+            string getInput;
+            double celcius, fahrenheit;
+            Console.Write("Enter Fahrenheit value:");
+            
+            getInput = Console.ReadLine();
+            fahrenheit = Convert.ToDouble(getInput);
+
+            celcius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine(fahrenheit.ToString("#.##") + " Fahrenheit in C is " + celcius.ToString("#.##")+" Celcius");
+
+        }
+    }
+}
